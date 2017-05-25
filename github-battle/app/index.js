93d57a3568e230e6a518545df8937be294324cc0
Noah Wisch <noah.wisch@gmail.com>
2017-05-25 10:45:40 -0400

    setting up webpack

---
+++ github-battle/app/index.js
@@ -0,0 +1,18 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+require('./style.css');
+
+// components are concerned w/ 3 things: state, lifecycle event, UI
+
+class App extends React.Component {
+  render() { // used to describe lifecycle of component
+    return(
+      <div>Hello World!</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
